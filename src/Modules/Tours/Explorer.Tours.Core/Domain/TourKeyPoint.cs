a50a7d0b2fb3e9fe06e7780ac0cb3f8ca6aa75ee
Uros Jevtic <uros.jevtic888@gmail.com>
2023-10-13 17:25:52 +0200

    Added model and dto

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourKeyPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Explorer.BuildingBlocks.Core.Domain;
+using Microsoft.Extensions.Options;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class TourKeyPoint : Entity
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string Image { get; init; }
+        public double Latitude { get; init; }
+        public double Longitude { get; init; }
+
+        public TourKeyPoint(string name, string description, string image, double latitude, double longitude)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+    }
+}
