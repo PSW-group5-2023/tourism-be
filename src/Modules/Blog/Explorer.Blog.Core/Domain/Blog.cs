ac10bebc84ce6d75779078548dec47407f66e622
leopoldina01 <leopoldinica123@gmail.com>
2023-10-13 15:56:11 +0200

    feat: Add model for Comments and Blogs

---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/Blog.cs
@@ -0,0 +1,27 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.Core.Domain
+{
+    public class Blog : Entity
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatingDate { get; private set; }
+
+        //pictures
+
+        public BlogStatus BlodStatus { get; private set; }
+    }
+
+    public enum BlogStatus 
+    { 
+        DRAFT,
+        PUBLISHED,
+        CLOSED 
+    }
+}
---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/Comment.cs
@@ -0,0 +1,35 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.Core.Domain
+{
+    public class Comment : Entity
+    {
+        public string Username { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public string Description { get; private set; }
+        public DateTime? LastEditDate { get; private set; }
+        public long BlogId { get; private set; }
+
+        public Comment(string username, DateTime creationDate, string description, DateTime? lastEditDate, long blogId)
+        {
+            Username = username;
+            CreationDate = creationDate;
+            Description = description;
+            LastEditDate = lastEditDate;
+            BlogId = blogId;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Username)) throw new ArgumentException("Invalid Name");
+            if (string.IsNullOrWhiteSpace(Description)) throw new ArgumentException("Invalid Name");
+        }
+    }
+}
