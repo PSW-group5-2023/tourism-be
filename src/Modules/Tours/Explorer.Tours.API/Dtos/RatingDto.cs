622e2c8dc3d73e0c71d941aef60ae88b3dd23315
Marko Kosarac <marko_kosarac@hotmail.com>
2023-10-14 13:25:40 +0200

    feat:Add model and dto for tour rating

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/RatingDto.cs
@@ -0,0 +1,20 @@
+﻿using Explorer.Stakeholders.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class RatingDto
+    {
+        public int Id { get; set; }
+        public int Mark { get; set; }
+        public string Comment { get; set; }
+        public Person Person { get; set; }
+        public DateTime DateOfVisit { get; set; }
+        public DateTime DateOfCommenting { get; set; }
+        public List<Uri> Images { get; set; }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Rating.cs
@@ -0,0 +1,44 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using Explorer.Stakeholders.Core.Domain;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class Rating : Entity
+    {
+        public int Mark { get; private set; }
+        public string Comment { get; private set; }
+        public Person Person { get; private set; }
+        public DateTime  DateOfVisit { get; private set; }
+        public DateTime DateOfCommenting { get; private set; }
+        public List<Uri> Images { get; private set; }
+
+        public Rating(int mark, string comment, Person person, DateTime dateOfVisit, DateTime dateOfCommenting, List<Uri> images)
+        {
+            Mark = mark;
+            Comment = comment;
+            Person = person;
+            DateOfVisit = dateOfVisit;
+            DateOfCommenting = dateOfCommenting;
+            Images = images;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Mark.ToString()) || Mark < 1 || Mark > 5) throw new ArgumentException("Invalid Mark");
+            if (string.IsNullOrWhiteSpace(Comment)) throw new ArgumentException("Invalid Comment");
+            if (Person == null) throw new ArgumentException("Invalid Person");
+            if (string.IsNullOrWhiteSpace(DateOfVisit.ToString())) throw new ArgumentException("Invalid DateOfVisit");
+            if (string.IsNullOrWhiteSpace(DateOfCommenting.ToString())) throw new ArgumentException("Invalid DateOfCommenting");
+            //if (Images.Count == 0) throw new ArgumentException("Invalid Images");
+        }
+    }
+}
