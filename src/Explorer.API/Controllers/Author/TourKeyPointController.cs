72676abab869fdb9d0d28a06001e360885f1fca4
Uros Jevtic <uros.jevtic888@gmail.com>
2023-10-14 17:09:38 +0200

    feat: Added key point controller

---
+++ src/Explorer.API/Controllers/Author/TourKeyPointController.cs
@@ -0,0 +1,45 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.UseCases.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Author
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/tourKeyPoint")]
+    public class TourKeyPointController : BaseApiController
+    {
+        private readonly ITourKeyPointService _tourKeyPointService;
+
+        public TourKeyPointController(ITourKeyPointService tourKeyPointService)
+        {
+            _tourKeyPointService = tourKeyPointService;
+        }
+
+
+        [HttpPost]
+        public ActionResult<TourKeyPointDto> Create([FromBody] TourKeyPointDto tourKeyPoint)
+        {
+            var result = _tourKeyPointService.Create(tourKeyPoint);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<TourKeyPointDto> Update([FromBody] TourKeyPointDto tourKeyPoint)
+        {
+            var result = _tourKeyPointService.Update(tourKeyPoint);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _tourKeyPointService.Delete(id);
+            return CreateResponse(result);
+
+        }
+    }
+}
