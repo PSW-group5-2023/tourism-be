7d9d8a1ef28bc36810485fdc956e23b0b9a75ce6
Filip Đokić <filipdjokic2001@gmail.com>
2023-10-14 13:02:53 +0200

    feat: Implementacija Entiteta, Dto-a i inicijalizacija Servisa i Interfejsa.

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/ClubDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos
+{
+    public class ClubDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Uri ClubPicture { get; set; }
+    }
+}
