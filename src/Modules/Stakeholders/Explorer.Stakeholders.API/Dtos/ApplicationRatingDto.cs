d5498328ffc048df669958b998c3cd067d8e87f5
Marko Radetic <markoradetic67@gmail.com>
2023-10-16 00:13:31 +0200

    feat: Added DTO and Service for ApplicationRating.

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/ApplicationRatingDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos
+{
+    public class ApplicationRatingDto
+    {
+        public int Grade { get; set; }
+        public string? Comment { get; set; }
+        public DateTime IssueDate { get; set; }
+        public long PersonId { get; set; }  
+
+    }
+}
