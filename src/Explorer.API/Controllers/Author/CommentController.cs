a2680dbe784bf9e2f940b6f95ab2c6f32b742eeb
leopoldina01 <leopoldinica123@gmail.com>
2023-10-16 17:49:01 +0200

    feat: Add Controller for Author

---
+++ src/Explorer.API/Controllers/Author/CommentController.cs
@@ -0,0 +1,40 @@
+﻿using Explorer.Blog.API.Dtos;
+using Explorer.Blog.API.Public;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Author
+{
+    //[Authorize(Policy = "authorPolicy")]
+    [Route("api/author/comment")]
+    public class CommentController : BaseApiController
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<CommentDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _commentService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<CommentDto> Create([FromBody] CommentDto commentDto)
+        {
+            var result = _commentService.Create(commentDto);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<CommentDto> Update([FromBody] CommentDto commentDto)
+        {
+            var result = _commentService.Update(commentDto);
+            return CreateResponse(result);
+        }
+    }
+}
