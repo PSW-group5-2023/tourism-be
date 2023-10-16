95c1a840347d28dbb70fcd6badc285e2880bba48
JovanKatanic <jovan.katanic0101@gmail.com>
2023-10-16 18:00:19 +0200

    fix: tests, added another controller for author, added current datetime when creating a blog, authorization

--- src/Explorer.API/Controllers/Tourist/BlogController.cs
+++ src/Explorer.API/Controllers/Tourist/BlogController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Explorer.API.Controllers.Tourist
 {
-    //[Authorize(Policy = "touristPolicy")]
+    [Authorize(Policy = "touristPolicy")]
     [Route("api/tourist/blog")]
     public class BlogController : BaseApiController
     {
