a16e4c156f651c41fc90ba207c6748bc498902cb
Nedeljko Babic <bnedeljko0@gmail.com>
2023-10-16 18:13:04 +0200

    added request domain and dto class

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/JoinRequestDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos
+{
+    public class JoinRequestDto
+    {
+        public long ClubId { get; set; }
+        public long UserId { get; set; }
+        public Status RequestStatus { get; set; }
+        public bool RequestDirection { get; set; }   // 0 is for guest -> owner direction
+
+    }
+}
+
+public enum Status
+{
+    Pending,
+    Accepted,
+    Declined
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/JoinRequest.cs
@@ -0,0 +1,36 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Explorer.Stakeholders.Core.Domain;
+
+
+public class Request : Entity
+{
+    public long ClubId { get; private set; }
+    public long UserId { get; private set; }
+    public Status RequestStatus { get; private set; }
+
+    public Request(long clubId, long userId, Status status)
+    {
+        if (clubId == 0) throw new ArgumentException("Invalid UserId");
+        if (userId == 0) throw new ArgumentException("Invalid UserId");
+
+        ClubId = clubId;
+        UserId = userId;
+        RequestStatus = status;
+
+    }
+
+}
+public enum Status
+{
+    Pending,
+    Accepted,
+    Declined
+}
