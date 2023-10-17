83c854fabcdceb18f43d63f7f7992fc36bec6e7a
Ana <anaranic01@gmail.com>
2023-10-17 11:51:32 +0200

    feat: Added Model, DTO, Service and Controller for TourProblem

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourProblemDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public enum TourProblemPriority
+    {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    public class TourProblemDto
+    {
+        public int Id { get; set; }
+        public long TourId { get; set; }
+        public string Category { get; set; }
+        public TourProblemPriority Priority { get; set; }
+        public string Description { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourProblem.cs
@@ -0,0 +1,44 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Explorer.Tours.Core.Domain
+{
+
+    public enum TourProblemPriority
+    {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    public class TourProblem : Entity
+    {
+        public long TourId { get; init; }
+        public string Category { get; init; }
+        public TourProblemPriority Priority { get; init; }
+        public string Description { get; init; }
+        public DateTime Time { get; init; }
+
+        public TourProblem(long tourId, string category, TourProblemPriority priority, string description, DateTime time)
+        {
+            TourId = tourId;
+            Category = category;
+            Priority = priority;
+            Description = description;
+            Time = time;
+            Validate();
+        }
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Category)) throw new ArgumentException("Invalid Category.");
+            if (string.IsNullOrWhiteSpace(Priority.ToString())) throw new ArgumentException("Invalid Priority.");
+            if (string.IsNullOrWhiteSpace(Description)) throw new ArgumentException("Invalid Description.");
+            if (string.IsNullOrWhiteSpace(Time.ToString())) throw new ArgumentException("Invalid Time.");
+        }
+    }
+}
