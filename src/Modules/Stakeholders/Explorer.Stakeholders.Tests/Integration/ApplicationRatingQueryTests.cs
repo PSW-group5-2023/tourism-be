cb2617047ae8ffbd7d31cfda8768a9a6bea9a579
Marko Radetic <markoradetic67@gmail.com>
2023-10-18 15:05:23 +0200

    test: Added Query Test for Application Rating controller.

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/ApplicationRatingQueryTests.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/ApplicationRatingQueryTests.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿using Explorer.API.Controllers.Administrator;
+using Explorer.API.Controllers.Administrator.Administration;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +16,34 @@ using System.Threading.Tasks;
 
 namespace Explorer.Stakeholders.Tests.Integration
 {
-    public class ApplicationRatingQueryTests
+    public class ApplicationRatingQueryTests : BaseStakeholdersIntegrationTest
     {
+        public ApplicationRatingQueryTests(StakeholdersTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Retrieves_all()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+
+            // Act
+            var result = ((ObjectResult)controller.GetAll(0, 0).Result)?.Value as PagedResult<ApplicationRatingDto>;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.Results.Count.ShouldBe(3);
+            result.TotalCount.ShouldBe(3);
+        }
+
+        [Fact]
+
+        private static ApplicationRatingController CreateController(IServiceScope scope)
+        {
+            return new ApplicationRatingController(scope.ServiceProvider.GetRequiredService<IApplicationRatingService>())
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
     }
 }
