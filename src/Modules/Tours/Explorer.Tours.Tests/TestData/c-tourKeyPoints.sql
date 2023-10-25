e87ba2b699d6e5c2fc73868fe151a10740cadcd4
JovanKatanic <jovan.katanic0101@gmail.com>
2023-10-25 23:23:57 +0200

    fix: tests for tour keypoints

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/c-tourKeyPoints.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/c-tourKeyPoints.sql
@@ -1,11 +1,11 @@
 ﻿INSERT INTO tours."TourKeyPoints"(
-    "Id", "Name", "Description", "Image", "Latitude", "Longitude")
-VALUES (-1, 'Tacka 1', 'Tacka 1 je prva tacka', 'http://tacka1.com/', 24.22, 12.3, 1);
+    "Id", "Name", "Description", "Image", "Latitude", "Longitude", "TourId")
+VALUES (-1, 'Tacka 1', 'Tacka 1 je prva tacka', 'http://tacka1.com/', 24.22, 12.3, -1);
 
 INSERT INTO tours."TourKeyPoints"(
-    "Id", "Name", "Description", "Image", "Latitude", "Longitude")
-VALUES (-2, 'Tacka 2', 'Tacka 2 je prva tacka', 'http://tacka2.com/', -24.22, -12.3, 1);
+    "Id", "Name", "Description", "Image", "Latitude", "Longitude", "TourId")
+VALUES (-2, 'Tacka 2', 'Tacka 2 je prva tacka', 'http://tacka2.com/', -24.22, -12.3, -1);
 
 INSERT INTO tours."TourKeyPoints"(
-    "Id", "Name", "Description", "Image", "Latitude", "Longitude")
-VALUES (-3, 'Tacka 3', 'Tacka 3 je prva tacka', 'http://tacka3.com/', -64.22, 82.3, 2);
+    "Id", "Name", "Description", "Image", "Latitude", "Longitude", "TourId")
+VALUES (-3, 'Tacka 3', 'Tacka 3 je prva tacka', 'http://tacka3.com/', -64.22, 82.3, -2);
