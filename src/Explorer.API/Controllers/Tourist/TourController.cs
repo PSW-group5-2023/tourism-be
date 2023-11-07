4e55c3cc751c1fb527401843428ae5bf6bad8f98
Jovan Jokić <jovanjokic555@gmail.com>
2023-11-07 20:10:40 +0100

    feat: Create Tour and TourKeypoint Controllers for tourist user

---
+++ src/Explorer.API/Controllers/Tourist/TourController.cs
@@ -0,0 +1,29 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Explorer.Tours.Core.UseCases;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/tourist/tours")]
+    public class TourController : BaseApiController
+    {
+        private readonly ITourService _tourService;
+
+        public TourController(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<TourDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _tourService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+    }
+}
