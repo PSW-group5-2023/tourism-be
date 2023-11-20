83821e7cb7ce87d7a4ed4ef8c4a5dfaf1bff91f1
Marko Radetic <markoradetic67@gmail.com>
2023-11-20 23:59:31 +0100

    feat: Added dto and mapper for Encounter.

---
+++ src/Explorer.Encounters.API/Dtos/EncounterDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.API.Dtos
+{
+    public class EncounterDto
+    {
+        public int Id { get; set; }
+        public int AdministratorId { get; set; }
+        public int TouristId { get; set; }
+        public string? Description { get; set; }
+        public string? Name { get; set; }
+        public double ExpiriencePoints { get; set; }
+        public int Status { get; set; }
+        public int Type { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+    }
+}
