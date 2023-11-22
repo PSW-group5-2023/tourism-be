7bcfdaa2f940c6c6dc5327c9442855962250d11a
JovanSarac <jovan_bn@outlook.com>
2023-11-23 00:00:26 +0100

    Add method for get number of session by tourId and statusSession for author

--- src/Explorer.API/Controllers/Author/Authoring/SessionController.cs
+++ src/Explorer.API/Controllers/Author/Authoring/SessionController.cs
@@ -1,5 +1,6 @@
 ﻿using Explorer.Tours.API.Dtos.Statistics;
 using Explorer.Tours.API.Public.Execution;
+using Explorer.Tours.Core.Domain.Sessions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,6 +29,13 @@ namespace Explorer.API.Controllers.Author.Authoring
         public ActionResult<List<TourStatisticsDto>> GetAbandonedStatistics()
         {
             var result = _sessionService.GetAbandonedStatistics();
+            return CreateResponse(result);
+        }
+
+        [HttpGet("getSessionsByStatusForTourStatistics/{tourId:int}/{status:int}")]
+        public ActionResult<TourStatisticsDto> GetSessionsByStatusForTourStatistics(int tourId,SessionStatus status)
+        {
+            var result = _sessionService.GetSessionsByStatusForTourStatistics(tourId, (int)status);
             return CreateResponse(result);
         }
     }
