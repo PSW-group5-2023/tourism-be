72780a4e7d169320c5a77671aa5c061c2b3efe0c
Aleksa Djukic <aleksadjukic25@gmail.com>
2023-12-06 15:56:15 +0100

    test: Add tests for ChallengeExecution

---
+++ src/Modules/Encounters/Explorer.Encounters.Tests/TestData/c-challengeexecutions.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO encounters."ChallengeExecutions"(
+	"Id", "TouristId", "ChallengeId", "Latitude", "Longitude", "ActivationTime", "CompletionTime")
+	VALUES (-1, -21, -1, 0, 0, '2023-10-17 23:05:27.893+02', null);
+
+INSERT INTO encounters."ChallengeExecutions"(
+	"Id", "TouristId", "ChallengeId", "Latitude", "Longitude", "ActivationTime", "CompletionTime")
+	VALUES (-2, -22, -1, 0, 0, '2023-10-17 23:05:27.893+02', null);
+
+INSERT INTO encounters."ChallengeExecutions"(
+	"Id", "TouristId", "ChallengeId", "Latitude", "Longitude", "ActivationTime", "CompletionTime")
+	VALUES (-3, -23, -1, 0, 0, '2023-10-17 23:05:27.893+02', null);
