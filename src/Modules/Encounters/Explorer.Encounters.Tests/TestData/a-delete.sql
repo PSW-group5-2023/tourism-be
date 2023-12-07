180c91667197bbc0be0712e806222aede9bc5d27
Aleksa Djukic <aleksadjukic25@gmail.com>
2023-12-07 18:59:25 +0100

    fix: fix test delete script for encounters

--- src/Modules/Encounters/Explorer.Encounters.Tests/TestData/a-delete.sql
+++ src/Modules/Encounters/Explorer.Encounters.Tests/TestData/a-delete.sql
@@ -1,4 +1,3 @@
-﻿DELETE FROM encounters."Challenges";
-DELETE FROM encounters."UserExperience";
 ﻿DELETE FROM encounters."ChallengeExecutions";
-
+DELETE FROM encounters."Challenges";
+DELETE FROM encounters."UserExperience";
