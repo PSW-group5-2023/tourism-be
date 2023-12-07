6cf238b020dce29a8b472ed311812ff8e3810fe1
Nevena Gligorov <114764987+Nevenaa01@users.noreply.github.com>
2023-12-07 18:49:51 +0100

    feat: added test for getAll

--- src/Modules/Payments/Explorer.Payments.Tests/Integration/PaymentRecordQueryTests.cs
+++ src/Modules/Payments/Explorer.Payments.Tests/Integration/PaymentRecordQueryTests.cs
@@ -1,6 +1,10 @@
 ﻿using Explorer.API.Controllers.Shopping;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Payments.API.Dtos;
 using Explorer.Payments.API.Public;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +17,17 @@ namespace Explorer.Payments.Tests.Integration
     {
         public PaymentRecordQueryTests(PaymentsTestFactory factory) : base(factory)
         {
+        }
+
+        public void Retrieves_all()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+
+            var result = ((ObjectResult)controller.GetAll(0, 0).Result)?.Value as PagedResult<PaymentRecordDto>;
+
+            result.ShouldNotBeNull();
+            result.Results.Count.ShouldBe(1);
         }
 
         private static PaymentRecordController CreateController(IServiceScope scope)
