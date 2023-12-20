cb489425da22dcab787aa13253fa1bf7b786e668
Jelena <jblanusa13@gmail.com>
2023-12-20 14:22:38 +0100

    feat: Added RecommenderService

---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/RecommenderService.cs
@@ -0,0 +1,25 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases
+{
+    public class RecommenderService 
+    {
+        private readonly ITourRepository _tourRepository;
+        private readonly IPreferencesRepository _preferencesRepository;
+        public RecommenderService(ITourRepository tourRepository, IPreferencesRepository preferencesRepository)
+        {
+            _tourRepository = tourRepository;
+            _preferencesRepository = preferencesRepository;
+        }
+
+   
+
+    }
+}
