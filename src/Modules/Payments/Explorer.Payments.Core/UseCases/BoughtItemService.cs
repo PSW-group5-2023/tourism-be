751ce96837d673bb8b2539e8b7fe8bcae37765d2
mareenah <kicanovicmarina@gmail.com>
2023-12-21 16:24:49 +0100

    fix: Implemented interface with method GetUsedByUserId

--- src/Modules/Payments/Explorer.Payments.Core/UseCases/BoughtItemService.cs
+++ src/Modules/Payments/Explorer.Payments.Core/UseCases/BoughtItemService.cs
@@ -163,5 +163,11 @@ namespace Explorer.Payments.Core.UseCases
                 return Result.Fail(FailureCode.InvalidArgument).WithError(e.Message);
             }
         }
+        public Result<List<BoughtItemDto>> GetUsedByUserId(int userId)
+        {
+            var boughtItems = shoppingCartRepository.GetAllByUserId(userId);
+            return MapToDto(boughtItems);
+        }
+
     }
 }
