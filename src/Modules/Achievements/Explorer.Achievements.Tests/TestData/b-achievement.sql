91abb00b8d1f01a3da8dbb95d4fc24dc55adae39
Strahinja Banjanac <strahinja.banjanac@gmail.com>
2024-05-12 11:01:07 +0200

    fix: insert script for testing

--- src/Modules/Achievements/Explorer.Achievements.Tests/TestData/b-achievement.sql
+++ src/Modules/Achievements/Explorer.Achievements.Tests/TestData/b-achievement.sql
@@ -1,11 +1,11 @@
 ﻿INSERT INTO achievements."Achievements"(
-    "Id", "Name", "Description", "Icon", "Rarity")
+    "Id", "Name", "Description", "Icon", "Rarity","KeypointId","AuthorId")
 VALUES (-1, 'Achievement1', 'Description1', 'https://example.com/icon1.png', 1, -16, -3);
 
 INSERT INTO achievements."Achievements"(
-    "Id", "Name", "Description", "Icon", "Rarity")
+    "Id", "Name", "Description", "Icon", "Rarity","KeypointId","AuthorId")
 VALUES (-2, 'Achievement2', 'Description2', 'https://example.com/icon2.png', 2, -10, -3);
 
 INSERT INTO achievements."Achievements"(
-    "Id", "Name", "Description", "Icon", "Rarity")
+    "Id", "Name", "Description", "Icon", "Rarity","KeypointId","AuthorId")
 VALUES (-3, 'Achievement2', 'Description2', 'https://example.com/icon2.png', 3, -5, -5);
