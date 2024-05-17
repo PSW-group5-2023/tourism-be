ef01d0e6e316dfa9d92c882606a40e97348259de
Σ <mboskovic420@gmail.com>
2024-05-17 23:28:09 +0200

    added:
    added dockerfile and dockercompose for frontend

---
+++ stacks/build/gateway/files/scripts/build-app.sh
@@ -0,0 +1,8 @@
+TOURISM_API_URL=$1
+TOURISM_WSS_URL=$2
+cd app || exit
+export API_HOST=${TOURISM_API_URL}
+export WSS_HOST=${TOURISM_WSS_URL}
+envsubst < environment.ts.template > ./src/env/environment.ts || exit
+npm run build
+cd dist && mv "$(find . -maxdepth 1 -type d | tail -n 1)" /app
