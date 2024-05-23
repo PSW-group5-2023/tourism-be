9410e55f022b246c633151ffb00cc0aa21bf28a6
Σ <mboskovic420@gmail.com>
2024-05-23 14:19:12 +0200

    fixed websocket connection from front through nginx to the backend

--- stacks/build/gateway/files/scripts/install-dependencies.sh
+++ stacks/build/gateway/files/scripts/install-dependencies.sh
@@ -4,7 +4,7 @@ set -e
 
 apk --update --no-cache add curl tar gettext
 
-GITHUB_TOKEN="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"
+GITHUB_TOKEN="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"
 FRONTEND_APP_SRC_URL=$1
 
 curl -L -o app.tar.gz -H "Authorization: token $GITHUB_TOKEN" "${FRONTEND_APP_SRC_URL}"
