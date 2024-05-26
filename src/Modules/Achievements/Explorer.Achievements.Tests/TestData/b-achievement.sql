0267b235666350cfacaf6d78c025d1a8d3b65bcc
Nevena Gligorov <114764987+Nevenaa01@users.noreply.github.com>
2024-05-27 00:20:22 +0200

    feat: tests for GetAllBaseAchievements() and GetAllComplexAchievements()

--- src/Modules/Achievements/Explorer.Achievements.Tests/TestData/b-achievement.sql
+++ src/Modules/Achievements/Explorer.Achievements.Tests/TestData/b-achievement.sql
@@ -3,9 +3,21 @@
 VALUES (-1, 'Achievement1', 'Description1', 'https://example.com/icon1.png', 1, '{{}}', -3);
 
 INSERT INTO achievements."Achievements"(
-    "Id", "Name", "Description", "Icon", "Rarity", "CraftingRecipe")
+    "Id", "Name", "Description", "Icon", "Rarity", "CraftingRecipe","AuthorId")
 VALUES (-2, 'Achievement2', 'Description2', 'https://example.com/icon2.png', 2, '{{}}', -3);
 
 INSERT INTO achievements."Achievements"(
-    "Id", "Name", "Description", "Icon", "Rarity", "CraftingRecipe")
+    "Id", "Name", "Description", "Icon", "Rarity", "CraftingRecipe","AuthorId")
 VALUES (-3, 'Achievement2', 'Description2', 'https://example.com/icon2.png', 3, '{{}}', -5);
+
+INSERT INTO achievements."Achievements"(
+    "Id", "Name", "Description", "Icon", "Rarity", "CraftingRecipe","AuthorId")
+VALUES (-4, 'Achievement4', 'Description4', 'https://example.com/icon2.png', 3, '{{}}', -5);
+
+INSERT INTO achievements."Achievements"(
+    "Id", "Name", "Description", "Icon", "Rarity", "CraftingRecipe","AuthorId")
+VALUES (-5, 'Achievement5', 'Description5', 'https://example.com/icon2.png', 3, '{{}}', -5);
+
+INSERT INTO achievements."Achievements"(
+    "Id", "Name", "Description", "Icon", "Rarity", "CraftingRecipe","AuthorId")
+VALUES (-6, 'Achievement6', 'Description6', 'https://example.com/icon2.png', 3, '{{-4, -5}}', -5);
