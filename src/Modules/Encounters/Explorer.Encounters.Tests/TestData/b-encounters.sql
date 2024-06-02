4bdc7858331b9e282dba00a189e3d1479fc8762b
Jovan Jokić <jovanjokic555@gmail.com>
2024-06-02 16:58:32 +0200

    fix: Fixing test data

--- src/Modules/Encounters/Explorer.Encounters.Tests/TestData/b-encounters.sql
+++ src/Modules/Encounters/Explorer.Encounters.Tests/TestData/b-encounters.sql
@@ -1,27 +1,27 @@
 ﻿INSERT INTO encounters."Encounters"(
-    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
-VALUES (-1, -3, 'Enjoy the Challenge by calling as many people as possible to join.', 'Community Connection Rally', 1, 0, 45.249055, 19.850548, 190, null, false, null, null, null, 50, 2);
+    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "AchievementId", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
+VALUES (-1, -3, 'Enjoy the Challenge by calling as many people as possible to join.', 'Community Connection Rally', 1, 0, 45.249055, 19.850548, 190, null, false, -1, null, null, null, 50, 2);
 INSERT INTO encounters."Encounters"(
-    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
-VALUES (-2, -4, 'To complete this challenge, you need to find the designated picture and be at that location for 30 seconds.', 'Visual Quest Adventure', 1, 1, 45.252909, 19.855888, 30, null, false, 'https://fajlovi.bos4.tours/uploads/2020/10/images/tour_217/Petrovaradinska%20tvrdjava%20sat.jpg', 45.253355, 19.861284, null, null);
+    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "AchievementId", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
+VALUES (-2, -4, 'To complete this challenge, you need to find the designated picture and be at that location for 30 seconds.', 'Visual Quest Adventure', 1, 1, 45.252909, 19.855888, 30, null, false, -1, 'https://fajlovi.bos4.tours/uploads/2020/10/images/tour_217/Petrovaradinska%20tvrdjava%20sat.jpg', 45.253355, 19.861284, null, null);
 INSERT INTO encounters."Encounters"(
-    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
-VALUES (-3, -3, 'To complete the challenge, you need to shout out loud.', 'Challenge 3', 0, 2, 45.255387, 19.845547, 20, null, false, null, null, null, null, null);
+    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "AchievementId", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
+VALUES (-3, -3, 'To complete the challenge, you need to shout out loud.', 'Challenge 3', 0, 2, 45.255387, 19.845547, 20, null, false, -1, null, null, null, null, null);
 INSERT INTO encounters."Encounters"(
-    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
-VALUES (-4, -3, 'You need to do 10 push-ups to complete the challenge.', 'Challenge 4', 1, 2, 45.244873, 19.841853, 10, null, false, null, null, null, null, null);
+    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "AchievementId", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
+VALUES (-4, -3, 'You need to do 10 push-ups to complete the challenge.', 'Challenge 4', 1, 2, 45.244873, 19.841853, 10, null, false, -1, null, null, null, null, null);
 INSERT INTO encounters."Encounters"(
-    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
-VALUES (-5, -4, 'Gathering Waves Challenge', 'Snapshot Scavenger Hunt', 1, 1, 45.249647, 19.825326, 10, null, false, 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Serbia-0268_-_Name_of_Mary_Parish_Church_(7344449164).jpg', 45.255128, 19.845097, null, null);
+    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "AchievementId", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
+VALUES (-5, -4, 'Gathering Waves Challenge', 'Snapshot Scavenger Hunt', 1, 1, 45.249647, 19.825326, 10, null, false, -1, 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Serbia-0268_-_Name_of_Mary_Parish_Church_(7344449164).jpg', 45.255128, 19.845097, null, null);
 INSERT INTO encounters."Encounters"(
-    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
-VALUES (-6, -21, 'To complete this challenge, you need to find the designated picture and be at that location for 30 seconds.', 'Gathering Waves Challenge', 1, 0, 45.264473, 19.825806, 10, null, false, null, null, null, 50, 2);
+    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "AchievementId", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
+VALUES (-6, -21, 'To complete this challenge, you need to find the designated picture and be at that location for 30 seconds.', 'Gathering Waves Challenge', 1, 0, 45.264473, 19.825806, 10, null, false, -1, null, null, null, 50, 2);
 INSERT INTO encounters."Encounters"(
-    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
-VALUES (-7, -11, 'To complete this challenge, do a backflip.', 'Challenge for acrobat', 1, 2, 45.25190843660657, 19.83714580535889, 100, NULL, false, NULL, NULL, NULL, null, NULL);
+    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "AchievementId", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
+VALUES (-7, -11, 'To complete this challenge, do a backflip.', 'Challenge for acrobat', 1, 2, 45.25190843660657, 19.83714580535889, 100, NULL, false, -1, NULL, NULL, NULL, null, NULL);
 INSERT INTO encounters."Encounters"(
-    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
-VALUES (-8, -1, 'To complete this challenge, you need to find the designated picture and be at that location for 30 seconds.',  'Find a new means of transportation.', 1, 1, 45.256048215418566, 19.833498001098633, 50, -1, false, 'https://novisad.travel/wp-content/uploads/2022/03/Brzi-voz-Novi-Sad_ACA_0103-scaled.jpg', 45.26538136482243, 19.829292297363285, null, NULL);
+    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "AchievementId", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
+VALUES (-8, -1, 'To complete this challenge, you need to find the designated picture and be at that location for 30 seconds.',  'Find a new means of transportation.', 1, 1, 45.256048215418566, 19.833498001098633, 50, -1, false, -1, 'https://novisad.travel/wp-content/uploads/2022/03/Brzi-voz-Novi-Sad_ACA_0103-scaled.jpg', 45.26538136482243, 19.829292297363285, null, NULL);
 INSERT INTO encounters."Encounters"(
-    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
-VALUES (-9, -21, 'Ubedi publiku da ti da lep aplauz ', 'Demo izazov', 1, 2, 45.247477347792135, 19.853885173788512, 50, -2, false, null, null, null, null, null);
+    "Id", "CreatorId", "Description", "Name", "Status", "Type", "Latitude", "Longitude", "ExperiencePoints", "CheckpointId", "IsMandatory", "AchievementId", "Image", "LocationLatitude", "LocationLongitude", "RangeInMeters", "RequiredAttendance")
+VALUES (-9, -21, 'Ubedi publiku da ti da lep aplauz ', 'Demo izazov', 1, 2, 45.247477347792135, 19.853885173788512, 50, -2, false, -1, null, null, null, null, null);
