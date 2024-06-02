8ddd73cfe7a62755539e5c47db188c172465441e
Jovan Jokić <jovanjokic555@gmail.com>
2024-06-02 16:16:01 +0200

    feat: Create Question service, controller and SubmittedAnswer domain class

---
+++ src/Explorer.API/Controllers/Author/QuestionController.cs
@@ -0,0 +1,41 @@
+﻿using Explorer.Encounters.API.Dtos;
+using Explorer.Encounters.API.Public;
+using Explorer.Encounters.Core.UseCases;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Author
+{
+    [Authorize(Policy ="authorPolicy")]
+    [Route("api/author/questions")]
+    public class QuestionController : BaseApiController
+    {
+        private readonly IQuestionService _questionService;
+
+        public QuestionController(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        [HttpPost]
+        public ActionResult<QuestionDto> Create([FromBody] QuestionDto questionDto)
+        {
+            var result = _questionService.Create(questionDto);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<QuestionDto> Update([FromBody] QuestionDto questionDto)
+        {
+            var result = _questionService.Update(questionDto);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _questionService.Delete(id);
+            return CreateResponse(result);
+        }
+    }
+}
---
+++ src/Modules/Encounters/Explorer.Encounters.API/Public/IQuestionService.cs
@@ -0,0 +1,18 @@
+﻿using Explorer.Encounters.API.Dtos;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.API.Public
+{
+    public interface IQuestionService
+    {
+        Result<QuestionDto> Update(QuestionDto questionDto);
+        Result<QuestionDto> Create(QuestionDto questionDto);
+        Result Delete(int id);
+        Result<List<QuestionDto>> GetAllByEncounterId(long encounterId);
+    }
+}
