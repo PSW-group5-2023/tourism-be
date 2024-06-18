246193a9442dd28703b215e62784ae972c5d02d0
JovanKatanic <jovan.katanic0101@gmail.com>
2024-06-18 19:30:11 +0200

    feat: pgadmin

---
+++ operations/scripts/destroy.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -s|--stage)
+    STAGE="$2"
+    shift
+    shift
+    ;;
+    *)    
+    POSITIONAL+=("$1")
+    shift
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
+
+STAGE=${STAGE:-dev}
+STACK_NAME="explorer_operations_${STAGE}"
+
+docker stack rm "${STACK_NAME}"
+docker network rm operations_network_${STAGE}
+docker service rm explorer_operations_${STAGE}_pgadmin
+
+ 
