f95ea90bc05a0bf1df4afa08ba86685c78763dca
Aleksa Djukic <aleksadjukic25@gmail.com>
2024-06-29 20:58:21 +0200

    test: Add new and refactor existing TourRating tests

--- src/Modules/Tours/Explorer.Tours.Tests/Integration/TourExecution/TourRating/TourRatingQueryTests.cs
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/TourExecution/TourRating/TourRatingQueryTests.cs
@@ -1,10 +1,12 @@
 ﻿using Explorer.API.Controllers.Tourist;
+using Explorer.API.Controllers.Author.Authoring;
 using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Tours.API.Dtos.Rating;
 using Explorer.Tours.API.Public.Rating;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
+using Explorer.Tours.API.Dtos.Statistics;
 
 namespace Explorer.Tours.Tests.Integration.TourExecution.TourRating
 {
@@ -18,7 +20,7 @@ namespace Explorer.Tours.Tests.Integration.TourExecution.TourRating
         {
             // Arrange
             using var scope = Factory.Services.CreateScope();
-            var controller = CreateController(scope);
+            var controller = CreateTouristController(scope);
 
             // Act
             var result = ((ObjectResult)controller.GetAll(0, 0).Result)?.Value as PagedResult<TourRatingDto>;
@@ -28,26 +30,31 @@ namespace Explorer.Tours.Tests.Integration.TourExecution.TourRating
             result.Results.Count.ShouldBe(2);
             result.TotalCount.ShouldBe(2);
         }
+
         [Fact]
-        public void RetrievesOne()
+        public void Retrieves_one()
         {
             //Arrange
             using var scope = Factory.Services.CreateScope();
-            var controller = CreateController(scope);
+            var controller = CreateTouristController(scope);
 
             //Act
-            var result = (ObjectResult)controller.Get(-1).Result;
+            var result = ((ObjectResult)controller.Get(-1).Result)?.Value as TourRatingDto;
 
             //Assert
-            result.ShouldNotBe(null);
-            result.StatusCode.ShouldBe(200);
+            result.ShouldNotBeNull();
+            result.PersonId.ShouldBe(-1);
+            result.TourId.ShouldBe(-1);
+            result.Mark.ShouldBe(4);
+            result.Comment.ShouldBe("Bilo je veoma dobro");
         }
+
         [Fact]
-        public void RetrievesOneFailedInvalidId()
+        public void Retrieves_one_fails_invalid_id()
         {
             //Arrange
             using var scope = Factory.Services.CreateScope();
-            var controller = CreateController(scope);
+            var controller = CreateTouristController(scope);
 
             //Act
             var result = (ObjectResult)controller.Get(-3).Result;
@@ -56,9 +63,65 @@ namespace Explorer.Tours.Tests.Integration.TourExecution.TourRating
             result.StatusCode.ShouldBe(404);
         }
 
-        private static TourRatingController CreateController(IServiceScope scope)
+        [Fact]
+        public void Retrieves_all_by_tour_id()
         {
-            return new TourRatingController(scope.ServiceProvider.GetRequiredService<ITourRatingService>())
+            //Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateTouristController(scope);
+
+            //Act
+            var result = ((ObjectResult)controller.GetByTourId(-1).Result)?.Value as List<TourRatingDto>;
+
+            //Assert
+            result.ShouldNotBeNull();
+            result.Count.ShouldBe(1);
+        }
+
+        [Fact]
+        public void Retrieves_one_by_person_id_and_tour_id()
+        {
+            //Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateTouristController(scope);
+
+            //Act
+            var result = ((ObjectResult)controller.GetByPersonIdAndTourId(-1, -1).Result)?.Value as TourRatingDto;
+
+            //Assert
+            result.ShouldNotBeNull();
+            result.PersonId.ShouldBe(-1);
+            result.TourId.ShouldBe(-1);
+            result.Mark.ShouldBe(4);
+            result.Comment.ShouldBe("Bilo je veoma dobro");
+        }
+
+        [Fact]
+        public void Retrieves_best_rated_statistics()
+        {
+            //Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateAuthorController(scope);
+
+            //Act
+            var result = ((ObjectResult)controller.GetBestRatedStatistics().Result)?.Value as List<TourStatisticsDto>;
+
+            //Assert
+            result.ShouldNotBeNull();
+            result.Count.ShouldBe(2);
+        }
+
+        private static Explorer.API.Controllers.Tourist.TourRatingController CreateTouristController(IServiceScope scope)
+        {
+            return new Explorer.API.Controllers.Tourist.TourRatingController(scope.ServiceProvider.GetRequiredService<ITourRatingService>())
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+
+        private static Explorer.API.Controllers.Author.Authoring.TourRatingController CreateAuthorController(IServiceScope scope)
+        {
+            return new Explorer.API.Controllers.Author.Authoring.TourRatingController(scope.ServiceProvider.GetRequiredService<ITourRatingService>())
             {
                 ControllerContext = BuildContext("-1")
             };
