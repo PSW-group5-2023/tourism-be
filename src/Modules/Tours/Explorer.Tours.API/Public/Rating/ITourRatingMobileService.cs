86beb2c85323ef9584fe05846e92a900d70a7146
Strahinja Banjanac <strahinja.banjanac@gmail.com>
2024-08-20 16:47:01 +0200

    feat: user can create tourrating, demo script fix, and tests for creating tourrating

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/Rating/ITourRatingMobileService.cs
@@ -0,0 +1,15 @@
+﻿using Explorer.Tours.API.Dtos.Rating;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Public.Rating
+{
+    public interface ITourRatingMobileService
+    {
+        Result<TourRatingMobileDto> Create(TourRatingMobileDto rating);
+    }
+}
--- src/Modules/Tours/Explorer.Tours.Core/Domain/Rating/TourRating.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Rating/TourRating.cs
@@ -4,17 +4,17 @@ namespace Explorer.Tours.Core.Domain.Rating
 {
     public class TourRating : Entity
     {
-        public long PersonId { get; init; }
+        public long UserId { get; init; }
         public long TourId { get; init; }
         public int Mark { get; private set; }
         public string Comment { get; private set; }
-        public DateTime DateOfVisit { get; private set; }
+        public DateTime? DateOfVisit { get; private set; }
         public DateTime DateOfCommenting { get; private set; }
-        public List<Uri> Images { get; private set; }
-
-        public TourRating(long personId, long tourId, int mark, string comment, DateTime dateOfVisit, DateTime dateOfCommenting, List<Uri> images)
+        public List<Uri>? Images { get; private set; }
+        public TourRating() { }
+        public TourRating(long userId , long tourId, int mark , string comment, DateTime? dateOfVisit , DateTime dateOfCommenting , List<Uri>? images)
         {
-            PersonId = personId;
+            UserId = userId;
             TourId = tourId;
             Mark = mark;
             Comment = comment;
@@ -28,7 +28,7 @@ namespace Explorer.Tours.Core.Domain.Rating
         {
             if (Mark < 1 || Mark > 5) throw new ArgumentException("Invalid Mark");
             if (string.IsNullOrWhiteSpace(Comment)) throw new ArgumentException("Invalid Comment");
-            if (string.IsNullOrWhiteSpace(DateOfVisit.ToString()) || (GetMilliseconds(DateOfVisit) > GetMilliseconds(DateTime.UtcNow))) throw new ArgumentException("Invalid DateOfVisit");
+            //if (string.IsNullOrWhiteSpace(DateOfVisit.ToString()) || (GetMilliseconds(DateOfVisit) > GetMilliseconds(DateTime.UtcNow))) throw new ArgumentException("Invalid DateOfVisit");
             if (string.IsNullOrWhiteSpace(DateOfCommenting.ToString()) || (GetMilliseconds(DateOfCommenting) > GetMilliseconds(DateTime.UtcNow))) throw new ArgumentException("Invalid DateOfCommenting");
             //if (Images.Count == 0) throw new ArgumentException("Invalid Images");
         }
