9a3e2020bd08127f6cbeae199325656626bf86f2
Strahinja Banjanac <strahinja.banjanac@gmail.com>
2024-08-20 10:42:14 +0200

    feat: delete user and his inventory

--- src/Explorer.API/Controllers/Tourist/UserInformationController.cs
+++ src/Explorer.API/Controllers/Tourist/UserInformationController.cs
@@ -3,6 +3,7 @@ using Explorer.Achievements.API.Public;
 using Explorer.Achievements.Core.UseCases;
 using Explorer.Stakeholders.API.Dtos;
 using Explorer.Stakeholders.API.Public;
+using FluentResults;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +24,15 @@ namespace Explorer.API.Controllers.Tourist
             var userId = HttpContext.User.Claims.FirstOrDefault(c => c.Type.Equals("id"));
             var result = _userInformaionService.GetMobile(Convert.ToInt32(userId.Value));
             return CreateResponse(result);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete() 
+        {
+            var userId = HttpContext.User.Claims.FirstOrDefault(c => c.Type.Equals("id"));
+            var result= _userInformaionService.Delete(Convert.ToInt32(userId.Value));
+            return CreateResponse(result);
+
         }
     }
 }
---
+++ src/Modules/Achievements/Explorer.Achievements.Core/UseCases/InventoryMobileInternalService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Explorer.Achievements.API.Dtos;
+using Explorer.Achievements.API.Internal;
+using Explorer.Achievements.Core.Domain;
+using Explorer.BuildingBlocks.Core.UseCases;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Achievements.Core.UseCases
+{
+    public class InventoryMobileInternalService : CrudService<InventoryDto, Inventory>, IInventoryMobileInternalService
+    {
+        public InventoryMobileInternalService(ICrudRepository<Inventory> crudRepository, IMapper mapper) : base(crudRepository, mapper)
+        {
+        }
+
+    }
+}
