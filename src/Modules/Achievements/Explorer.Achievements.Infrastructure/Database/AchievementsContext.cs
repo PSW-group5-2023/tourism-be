28a170ecf73814df09356f80053d1680caa436ef
Strahinja Banjanac <strahinja.banjanac@gmail.com>
2024-08-27 12:14:07 +0200

    fix: fix migration warning

--- src/Modules/Achievements/Explorer.Achievements.Infrastructure/Database/AchievementsContext.cs
+++ src/Modules/Achievements/Explorer.Achievements.Infrastructure/Database/AchievementsContext.cs
@@ -1,6 +1,7 @@
 ﻿using Explorer.Achievements.Core.Domain;
 using Explorer.BuildingBlocks.Core.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,12 +21,18 @@ namespace Explorer.Achievements.Infrastructure.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            var comparer = new ValueComparer<Dictionary<int, int>>(
+                                    (c1, c2) => c1.SequenceEqual(c2),
+                                    c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.Key.GetHashCode(), v.Value.GetHashCode())),
+                                    c => c.ToDictionary(entry => entry.Key, entry => entry.Value)
+                                );
             modelBuilder.HasDefaultSchema("achievements");
             modelBuilder.Entity<Inventory>().Property(p => p.Achievements)
                      .HasColumnType("jsonb")
                      .HasConversion(
                        v => JsonSerializer.Serialize(v, new JsonSerializerOptions(JsonSerializerDefaults.General)),
-                       v => JsonSerializer.Deserialize<Dictionary<int, int>>(v, new JsonSerializerOptions(JsonSerializerDefaults.General))!);
+                       v => JsonSerializer.Deserialize<Dictionary<int, int>>(v, new JsonSerializerOptions(JsonSerializerDefaults.General))!
+                       ).Metadata.SetValueComparer(comparer); 
         }
     }
 }
