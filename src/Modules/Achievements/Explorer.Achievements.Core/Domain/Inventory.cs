33bf79ab54a5f128d59bd46b8919e0d43d4ccc13
Strahinja Banjanac <strahinja.banjanac@gmail.com>
2024-08-27 12:10:28 +0200

    fix: change achievements in inventory and fix complex achievements

--- src/Modules/Achievements/Explorer.Achievements.Core/Domain/Inventory.cs
+++ src/Modules/Achievements/Explorer.Achievements.Core/Domain/Inventory.cs
@@ -1,6 +1,8 @@
 ﻿using Explorer.BuildingBlocks.Core.Domain;
+using Microsoft.EntityFrameworkCore.Metadata;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,20 +12,33 @@ namespace Explorer.Achievements.Core.Domain
     public class Inventory:Entity
     {
         public long UserId { get; private set; }
-        public List<int> AchievementsId { get; set;}
+        [Column("Achievements", TypeName = "jsonb")]
+        public Dictionary<int,int> Achievements { get;  set; }
+        //public List<int> AchievementsId { get; set;}
         public Inventory() 
         {
-            AchievementsId = new List<int>();
+            Achievements = new Dictionary<int, int>();
         }
-        public Inventory(long userId, List<int> achievements) 
+        public Inventory(long userId, Dictionary<int, int> achievements) 
         {
             UserId = userId;
-            AchievementsId = achievements;
+            Achievements = achievements;
         }
         public void AddAchievementToInventory(int achievementId)
         {
-            if(!AchievementsId.Contains(achievementId))
-                AchievementsId.Add(achievementId);
+            if(Achievements.ContainsKey(achievementId))
+                Achievements[achievementId]++;
+            else
+                Achievements.Add(achievementId, 1);
+        }
+        public void AddComplexAchievementToInventory(int achievementId, List<int> requiredAchievements)
+        {
+            foreach (var ra in requiredAchievements)
+            {
+                if (Achievements.ContainsKey((int)ra) && Achievements[ra]>0)
+                    Achievements[ra]--;
+            }
+            AddAchievementToInventory(achievementId);
         }
     }
 }
--- src/Modules/Achievements/Explorer.Achievements.Infrastructure/Database/AchievementsContext.cs
+++ src/Modules/Achievements/Explorer.Achievements.Infrastructure/Database/AchievementsContext.cs
@@ -1,9 +1,11 @@
 ﻿using Explorer.Achievements.Core.Domain;
+using Explorer.BuildingBlocks.Core.Domain;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Explorer.Achievements.Infrastructure.Database
@@ -19,6 +21,11 @@ namespace Explorer.Achievements.Infrastructure.Database
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasDefaultSchema("achievements");
+            modelBuilder.Entity<Inventory>().Property(p => p.Achievements)
+                     .HasColumnType("jsonb")
+                     .HasConversion(
+                       v => JsonSerializer.Serialize(v, new JsonSerializerOptions(JsonSerializerDefaults.General)),
+                       v => JsonSerializer.Deserialize<Dictionary<int, int>>(v, new JsonSerializerOptions(JsonSerializerDefaults.General))!);
         }
     }
 }
