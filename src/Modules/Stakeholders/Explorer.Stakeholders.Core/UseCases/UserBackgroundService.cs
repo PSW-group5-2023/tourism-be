6f5d7a26ab7bb5162c13683315686169e6835c70
Strahinja Banjanac <strahinja.banjanac@gmail.com>
2024-09-05 20:57:21 +0200

    fix(backgroundService): exception handling

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/UseCases/UserBackgroundService.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/UseCases/UserBackgroundService.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,36 +14,55 @@ namespace Explorer.Stakeholders.Core.UseCases
     public class UserBackgroundService : BackgroundService
     {
         private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly ILogger<UserBackgroundService> _logger;
 
-        public UserBackgroundService(IServiceScopeFactory serviceScopeFactory)
+
+        public UserBackgroundService(IServiceScopeFactory serviceScopeFactory, ILogger<UserBackgroundService> logger)
         {
             _serviceScopeFactory = serviceScopeFactory;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                await CleanUpAccounts(stoppingToken);
-                await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    await CleanUpAccounts(stoppingToken);
+                    await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred during background execution.");
             }
         }
 
         private async Task CleanUpAccounts(CancellationToken stoppingToken)
         {
-            using (var scope = _serviceScopeFactory.CreateScope())
+            try
             {
-                var userRepository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
-                var usersQuery = await userRepository.GetAllGuestAsync();
-                var usersToDelete = await usersQuery
-                    .Where(u => (DateTime.UtcNow - u.GuestDateTimeCreated) >= TimeSpan.FromDays(14))
-                    .ToListAsync(stoppingToken);
-
-                if (usersToDelete.Any())
+                using (var scope = _serviceScopeFactory.CreateScope())
                 {
-                    await userRepository.DeleteGuestsAsync(usersToDelete, stoppingToken);
+                    var userRepository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
+                    var usersQuery = await userRepository.GetAllGuestAsync();
+                    var usersToDelete = await usersQuery
+                        .Where(u => (DateTime.UtcNow - u.GuestDateTimeCreated) >= TimeSpan.FromDays(14))
+                        .ToListAsync(stoppingToken);
+
+                    if (usersToDelete.Any())
+                    {
+                        await userRepository.DeleteGuestsAsync(usersToDelete, stoppingToken);
+                        _logger.LogInformation($"{usersToDelete.Count} guest accounts were deleted.");
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while cleaning up accounts.");
+            }
         }
+
     }
 }
